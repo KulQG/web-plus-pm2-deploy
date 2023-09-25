83717bc4bfdea6e388b38db532f7e38b0856b7b4
KulQG <cool.egor-kot@yandex.ru>
2023-09-25 16:50:01 +0300

    eco front

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -10,11 +10,14 @@ const {
 module.exports = {
   apps: [],
   deploy: {
-    user: DEPLOY_USER,
-    host: DEPLOY_HOST,
-    ref: DEPLOY_REF,
-    repo: "https://github.com/KulQG/web-plus-pm2-deploy",
-    path: DEPLOY_PATH,
-    "post-deploy": "cd ~/dev/frontend/source/frontend/ && npm i && npm run build",
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: "https://github.com/KulQG/web-plus-pm2-deploy",
+      path: DEPLOY_PATH,
+      "post-deploy":
+        "cd ~/dev/frontend/source/frontend/ && npm i && npm run build",
+    },
   },
 };
